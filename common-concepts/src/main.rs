558e95d156a39cd4b02cc18e5e76b9d760e01b1e
thisbok <thisbok@gmail.com>
2024-01-09 03:40:37 +0900

    variables and mutabillity

---
+++ common-concepts/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    // let mut x = 5;
+    // println!("The value of x is : {x}");
+    // x = 6;
+    // println!("The value of x is : {x}");
+
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // println!("Constants : {THREE_HOURS_IN_SECONDS}")
+
+    // shadowing
+    let x = 5;
+    println!("The value of x is : {x}");
+
+    let x = x + 1;
+    
+    {
+        let x = x * 2;
+        println!(" The value of x  in the inner scope is : {x}");
+    }
+
+    println!("The value of x is : {x}");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("The value of space is : {spaces}");
+
+    let mut spaces = "    ";
+    let spaces = spaces.len();
+     println!("The value of space is : {spaces}");
+}
