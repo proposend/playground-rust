6f703863ef177fa4e6a586fe63e82ddbf6594315
thisbok <thisbok@gmail.com>
2024-01-21 21:21:30 +0900

    Chapter6-3 Concise Control Flow with if let

---
+++ chapter6-3_concise-control-flow/src/main.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+enum UsState {
+    California,
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+fn main() {
+    {
+        let config_max = Some(3u8);
+        match config_max {
+            Some(max) => println!("The maximum is configured to be {}", max),
+            _ => (),
+        }
+    }
+    {
+        let config_max = Some(3u8);
+        if let Some(max) = config_max {
+            println!("The maximum is configured to be {}", max);
+        }
+    }
+    {
+        let mut count = 0;
+        // let coin = Coin::Quarter(UsState::California);
+        let coin = Coin::Dime;
+
+        match coin {
+            Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+            _ => count += 1,
+        }
+
+        println!("count: {}", count);
+    }
+    {
+        let mut count = 0;
+        let coin = Coin::Quarter(UsState::California);
+        if let Coin::Quarter(state) = coin {
+            println!("State quarter from {:?}!", state);
+        } else {
+            count += 1;
+        }
+    }
+}
