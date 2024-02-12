0f225be60e2fbf58f363e6ad0c104efdbbdbba7e
thisbok <thisbok@gmail.com>
2024-02-12 19:35:56 +0900

    Chapter12_3 Refactoring to Improve Modularity and Error Handling

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,48 @@
+use std::error::Error;
+use std::fs;
+
+// Grouping Configuration Values
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+// Creating a Constructor for Config
+impl Config {
+    // fn new(args: &[String]) -> Config {
+    //     // Improving the Error Message
+    //     if args.len() < 3 {
+    //         panic!("not enough arguments");
+    //     }
+
+    //     let query = args[1].clone();
+    //     let file_path = args[2].clone();
+
+    //     Config { query, file_path }
+    // }
+
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+// Extracting the Argument Parser
+// fn parse_config(args: &[String])-> Config {
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
+//
+//     Config {query, file_path}
+// }
