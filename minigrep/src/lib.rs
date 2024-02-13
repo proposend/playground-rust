737cf0b7aa2fd1b850cc7332f973e8c389bd4721
thisbok <thisbok@gmail.com>
2024-02-14 00:18:40 +0900

    chapter12_5 Working with Environment Variables

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 // Grouping Configuration Values
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 // Creating a Constructor for Config
@@ -29,14 +30,25 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{line}");
     }
 
@@ -56,6 +68,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
