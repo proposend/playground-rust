4e270727368a478791454184eea167a0749d7da5
thisbok <thisbok@gmail.com>
2024-03-18 23:07:23 +0900

    chapter20_2 Turning Our Single-Threaded Server into a Multithreaded Server

---
+++ chapter20_2/src/main.rs
@@ -0,0 +1,51 @@
+use std::{fs, thread};
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+
+use chapter20_2::ThreadPool;
+
+fn main() {
+    // Listening to the TCP Connection
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // Creating a Finite Number of Threads
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+
+        // Spawning a Thread for Each Request
+        let stream = stream.unwrap();
+
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    // Simulating a Slow Request in the Current Server Implementation
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
